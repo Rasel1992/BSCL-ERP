365e279fdebab39160c9f5f33f19a08d9bf1af3a
sohana <borna350@gmail.com>
2021-01-05 00:54:01 +0600

    summary details

--- app/Http/Controllers/Admin/InventoryController.php
+++ app/Http/Controllers/Admin/InventoryController.php
@@ -37,7 +37,6 @@ class InventoryController extends Controller
     public function summary()
     {
         try {
-//            $data['categories'] = Category::with('inventories')->get();
             $data['categories_head'] = Category::with(['inventories' => function($q) {
                 $q->where('location','hq');
             }])
@@ -50,12 +49,28 @@ class InventoryController extends Controller
                 $q->where('location','gs2');
             }])
                 ->get();
-//            dd($data);
             return view('admin.inventory.summary', compact( 'data'));
         } catch (\Exception $e) {
             return redirect()->back()->withErrors(['error' => $e->getMessage()]);
         }
     }
+
+//    public function summaryShow(Request $request)
+//    {
+//        try {
+//            $data['categoryData'] = Category::where('parent_id', 0)->with('nested')->get();
+//            $sql = Category::orderBy('categories.id', 'DESC');
+//            $sql->select('categories.*', 'B.category_name AS parent_name', 'C.category_name AS parent_mother', \DB::raw('IFNULL(D.subCount,0) AS subCount'));
+//            $sql->leftJoin('categories AS B', 'B.id','=','categories.parent_id');
+//            $sql->leftJoin('categories AS C', 'C.id','=','B.parent_id');
+//            $sql->leftJoin(\DB::raw('(SELECT parent_id, COUNT(id) AS subCount FROM categories GROUP BY parent_id) AS D'), 'categories.id','=','D.parent_id');
+//            $data['categories'] = $sql->get();
+//            $inventories = Inventory::latest()->paginate(10);
+//            return view('admin.inventory.index', compact('data', 'inventories'));
+//        } catch (\Exception $e) {
+//            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+//        }
+//    }
 
     /**
      * Show the form for creating a new resource.
---
+++ resources/views/admin/inventory/summary-show.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.backend')
+
+@section('title')
+    Inventories
+@endsection
+
+@section('content')
+    <section class="content">
+        <div class="panel">
+            <div class="box-header with-border">
+                <h3 class="box-title">Inventories</h3>
+                <div class="box-tools pull-right">
+                    <a class="button add" href="{{ route('admin.inventories.summary') }}">Summary</a>
+                    <a href="{{ route('admin.inventories.create') }}" class="button add"> Add Inventory</a>
+                </div>
+            </div> <!-- /.box-header -->
+            <div class="panel-body">
+                <table class="table table-hover table-2nd-no-sort" id="file_export">
+                    <thead>
+                    <tr>
+                        <th>SL</th>
+                        <th>Asset Code</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                        <th>Allocate To</th>
+                        <th>Voucher No</th>
+                        <th>Qty</th>
+                        <th>Cost</th>
+                        <th>Location</th>
+                        <th>Purchase Date</th>
+                        <th> </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($inventories as $key => $inventory)
+                        <tr>
+                            <td> {{$key + $inventories->firstItem()}}</td>
+                            <td> {{ $inventory->asset_code  }}</td>
+                            <td> {{ $inventory->description }}</td>
+                            <td> {{ $inventory->category->category_name }}</td>
+                            <td>
+                                @if($inventory->assign_to == 'user') <strong>Person:</strong><br> {{$inventory->user->name }}
+                                @else
+                                    <strong>Department:</strong><br> {{ $inventory->department->department}}
+                                @endif
+                            </td>
+
+                            <td> {{ $inventory->voucher_no }}</td>
+                            <td> {{ $inventory->qty }}</td>
+                            <td> {{ $inventory->cost }}</td>
+                            <td> @if($inventory->location == 'hq') Head Quarter @elseif($inventory->location == 'gs1') GS Gazipur @else GS Bethbunia @endif</td>
+                            <td>{{ $inventory->purchase_date}}</td>
+                            <td class="row-options text-muted small">
+                                <a href="{{route('admin.inventories.edit', $inventory->id) }}" class="ajax-modal-btn"><i data-toggle="tooltip" data-placement="top" title="Edit" class="fa fa-edit"></i></a>&nbsp;
+                                <form method="POST" action="{{ route('admin.inventories.destroy', $inventory->id) }}" accept-charset="UTF-8" class="data-form">
+                                    @csrf
+                                    @method('delete')
+                                    <a href="javascript:void(0)" @click="destroy" class="confirm ajax-silent" title="Trash" data-toggle="tooltip" data-placement="top"><i class="fa fa-trash-o"></i></a>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                @if($inventories->total())
+                    <div class="row">
+                        <div class="col-sm-5">
+                            <div class="dataTables_info" id="sortable_info" role="status" aria-live="polite">
+                                showing {{ $inventories->firstItem() }} to {{ $inventories->lastItem() }} of {{ $inventories->total() }} entries
+                            </div>
+                        </div>
+                        <div class="col-sm-7">
+                            <div class="dataTables_paginate paging_simple_numbers" id="sortable_paginate">
+                                {{ $inventories->links() }}
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            </div> <!-- /.box-body -->
+        </div> <!-- /.box -->
+        @include('admin.inventory.form_import')
+    </section>
+@endsection
+
+@push('scripts')
+    <script>
+        new Vue({
+            el: '#app',
+            methods: {
+                destroy: function () {
+                    const $this = $(event.target);
+
+                    Swal.fire({
+                        title: 'Are you sure?',
+                        text: "You won't be able to revert this!",
+                        type: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+                        if (result.value) {
+                            $this.closest('form').submit();
+                        }
+                    });
+                }
+            }
+        })
+    </script>
+@endpush
+
